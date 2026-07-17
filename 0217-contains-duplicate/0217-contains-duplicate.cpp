9713eebf2de51582dfd892b34d767694ed341c4e
Suchi Goel <suchig946@gmail.com>
2026-07-17 18:56:06 +0530

    Time: 81 ms (15.25%), Space: 111.4 MB (16.27%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums)
+     {
+        unordered_map<int, int> seen;
+        for (int num : nums) {
+            if(seen[num] >= 1)
+            return true;
+            seen[num]++;
+
+        }
+        return false ;
+        
+        
+    }
+};
