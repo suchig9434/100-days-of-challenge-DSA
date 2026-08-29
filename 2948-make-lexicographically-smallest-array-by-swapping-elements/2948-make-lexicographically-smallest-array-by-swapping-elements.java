d96df4091e4229d3085b80d47308086147989970
Suchi Goel <suchig946@gmail.com>
2026-08-29 11:42:33 +0530

    Time: 84 ms (65.91%), Space: 117.7 MB (98.48%) - LeetHub

---
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int n = nums.length;
+
+        // Store {value, originalIndex}
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        // Sort by value
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int[] ans = new int[n];
+
+        int start = 0;
+
+        while (start < n) {
+            int end = start;
+
+            // Find the complete connected group
+            while (end + 1 < n &&
+                   (long) arr[end + 1][0] - arr[end][0] <= limit) {
+                end++;
+            }
+
+            // Get original indices of this group
+            List<Integer> indices = new ArrayList<>();
+
+            for (int i = start; i <= end; i++) {
+                indices.add(arr[i][1]);
+            }
+
+            // Smallest index gets smallest value
+            Collections.sort(indices);
+
+            for (int i = start; i <= end; i++) {
+                ans[indices.get(i - start)] = arr[i][0];
+            }
+
+            start = end + 1;
+        }
+
+        return ans;
+    }
+}
